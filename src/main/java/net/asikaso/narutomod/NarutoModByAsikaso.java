c705f332e4fd0e216608944227265f9d8ded1b4b
Asikaso <yigitkogacak99@gmail.com>
2024-05-08 16:45:37 +0300

    Initial commit

---
+++ src/main/java/net/asikaso/narutomod/NarutoModByAsikaso.java
@@ -0,0 +1,17 @@
+package net.asikaso.narutomod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NarutoModByAsikaso implements ModInitializer {
+	public static final String MOD_ID = "narutomodasi";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Naruto world!");
+	}
+}
---
+++ src/main/java/net/asikaso/narutomod/NarutoModByAsikasoClient.java
@@ -0,0 +1,10 @@
+package net.asikaso.narutomod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NarutoModByAsikasoClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
